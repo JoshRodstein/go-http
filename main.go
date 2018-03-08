06ad3da5ef72cacae4b0c63117c916f2773fd72b
Josh Rodstein <joshuarodstein@gmail.com>
2018-03-08 12:09:39 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+/*
+* Sample API with GET and POST endpoint.
+* POST data is converted to string and saved in internal memory.
+* GET endpoint returns all strings in an array.
+ */
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+)
+
+type User struct {
+		Username string
+}
+
+var (
+	// flagPort is the open port the application listens on
+	flagPort = flag.String("port", "9000", "Port to listen on")
+)
+
+func main() {
+
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/get", GetHandler)
+	mux.HandleFunc("/post", PostHandler)
+
+	log.Printf("listening on port %s", *flagPort)
+	log.Fatal(http.ListenAndServe(":"+*flagPort, mux))
+}
+
+
+
+// GetHandler handles the index route
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+		user := User{}
+
+		// decode json request data into User struct
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+				http.Error(w, "json body empty", 200)
+				return
+		}
+
+		// marchal data from user struct back into json
+		// for writing
+		userJson, err := json.Marshal(user)
+		if err != nil {
+			http.Error(w, "Error converting results to json", 400)
+			return
+		}
+
+		fmt.Fprint(w, "jason data parsed and recieved\n:")
+		w.Write(userJson)
+		fmt.Fprint(w, "\n")
+}
+
+// PostHandler converts post request body to string
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		fmt.Fprint(w, "Post recieved\n")
+	}
+
+	user := User{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+			http.Error(w, "json body empty", 200)
+			return
+	}
+	userJson, err := json.Marshal(user)
+	if err != nil {
+		http.Error(w, "Error converting results to json", 400)
+		return
+	}
+
+	fmt.Fprint(w, "Did a post\n:")
+	w.Write(userJson)
+	fmt.Fprint(w, "\n")
+}
+
+
+
+func init() {
+	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+	flag.Parse()
+}
